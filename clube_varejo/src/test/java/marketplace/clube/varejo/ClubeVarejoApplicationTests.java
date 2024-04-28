efa44e3b9315da821fceaa0b681fba00a76cf7ef
Gabriel <gdeamachado@gmail.com>
2024-04-28 19:06:20 -0300

    profiles

--- clube_varejo/src/test/java/marketplace/clube/varejo/ClubeVarejoApplicationTests.java
+++ clube_varejo/src/test/java/marketplace/clube/varejo/ClubeVarejoApplicationTests.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -22,6 +23,7 @@ import marketplace.clube.varejo.controller.AcessoController;
 import marketplace.clube.varejo.model.Acesso;
 import marketplace.clube.varejo.repository.AcessoRepository;
 
+@Profile("test")
 @SpringBootTest(classes = ClubeVarejoApplication.class)
 class ClubeVarejoApplicationTests extends TestCase{
 
